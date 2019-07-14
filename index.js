5959d66ac09efca751bfe80b073386162d70c169
Rohan Salunke <rohans16996@gmail.com>
2019-07-14 21:35:22 +0530

    Tweak

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 let oTitle = [
-    { label: `Hello World!`, noOfBreaks: 0, domId: "titleLoad1" },
+    { label: `Hello World.`, noOfBreaks: 0, domId: "titleLoad1" },
     { label: `I'm ROHAN!`, noOfBreaks: 1, domId: "titleLoad2" },
     { label: `Passionate Full Stack Developer | Blogger`, noOfBreaks: 1, domId: "titleLoad3" },
     // { label: `- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - `, noOfBreaks: 2, domId: "titleLoad4" },
@@ -10,7 +10,7 @@ let oTitle = [
     { label: `- Front End Engineering `, noOfBreaks: 1, domId: "titleLoad4" },
     { label: `- Data Intensive Applications `, noOfBreaks: 1, domId: "titleLoad4" },
     { label: `- RnD and Software Architecture  `, noOfBreaks: 1, domId: "titleLoad4" },
-    { label: `- Feel free to Connect on Social Handles given below! `, noOfBreaks: 2, domId: "titleLoad4" },
+    { label: `- Feel free to Connect! `, noOfBreaks: 2, domId: "titleLoad4" },
 
 ];
 
